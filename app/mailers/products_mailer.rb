23886dcd169fd08b1d7ad97efd99d9c9206bb03a
KhrystynaInzhuvatova <kinzhuvatova@gmail.com>
2022-01-09 02:22:11 +0200

    csv4

--- app/mailers/products_mailer.rb
+++ app/mailers/products_mailer.rb
@@ -1,6 +1,6 @@
 class ProductsMailer < ApplicationMailer
   def all_products(csv)
-    attachments['products.csv'] = File.read(Rails.root.join('upload', 'file.csv'))
+    attachments['products.zip'] = ActiveSupport::Gzip.compress(File.read(Rails.root.join('upload', 'file.csv')))
     mail(to: "kinzhuvatova@gmail.com", from: "kinzhuvatova@gmail.com", subject: "products")
   end
 end
